81f9d95852587782d05d7a61acae3827a1823895
ElnurBDa <elnur.badalov.std@bhos.edu.az>
2023-04-12 19:41:04 +0400

    ready+-

--- client-app/src/components/redeem.tsx
+++ client-app/src/components/redeem.tsx
@@ -1,10 +1,49 @@
 import './style.css'
+import { FaBook, FaPhone, FaDollarSign } from 'react-icons/fa'
+
+
+const presents = [
+    {
+        title: "Kitab kuponu",
+        logo: FaBook,
+        price: 15
+    },
+    {
+        title: "Kontur",
+        logo: FaPhone,
+        price: 5
+    },
+    {
+        title: "Pul",
+        logo: FaDollarSign,
+        price: 1
+    },
+]
+
+
 
 export default function Redeem() {
+
     
     return (<>
     <div style={{flex:'1', minHeight:'200px'}} className='card'>
-        <h1 style={{textAlign:'center'}}>Redeem</h1>
+        <h1 style={{textAlign:'center', marginBottom:'50px'}}>Redeem</h1>
+        <section style={{display:'flex'}}>
+        {presents.map((present, index) => (
+            <div key={index} style={{
+                textAlign:'center',
+                flex:'1',
+                border:'2px solid blue', 
+                maxWidth:'150px', 
+                margin:'auto', 
+                padding:'10px',
+                borderRadius:'20%'}}>
+                <h3>{present.title}</h3>
+                <present.logo style={{fontSize:'60px', color:'blue'}}/>
+                <p>Qiymet: {present.price}</p>
+            </div>
+        ))}
+        </section>
     </div>
     </>)
 }
